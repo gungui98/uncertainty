cce8db25ac470a3e71f0903800d4026a5bbfcb04
Phi Nguyễn <phi.nguyen.uet@gmail.com>
2022-10-14 20:31:46 +0700

    assess dataset

--- crisp_uncertainty/data/camus/dataset.py
+++ crisp_uncertainty/data/camus/dataset.py
@@ -451,36 +451,36 @@ if __name__ == "__main__":
     print(samples.mean())
     print(samples.std())
 
-    if params.predict:
-        patient = ds[random.randint(0, len(ds) - 1)]
-        instant = patient.views[View.A4C]
-        img = instant.img_proc
-        gt = instant.gt_proc
-        print("Image shape: {}".format(img.shape))
-        print("GT shape: {}".format(gt.shape))
-        print("ID: {}".format(patient.id))
-
-        slice = random.randint(0, len(img) - 1)
-        img = img[slice].squeeze()
-        gt = gt[slice]
-    else:
-        sample = ds[random.randint(0, len(ds) - 1)]
-        img = sample[CamusTags.img].squeeze()
-        gt = sample[CamusTags.gt]
-        print("Image shape: {}".format(img.shape))
-        print("GT shape: {}".format(gt.shape))
-
-    print(img.min())
-    print(img.max())
-    print(img.mean())
-    print(img.std())
-
-    f, (ax1, ax2) = plt.subplots(1, 2)
-    ax1.imshow(img)
-    ax2.imshow(gt)
-    plt.show(block=False)
-
-    plt.figure(2)
-    plt.imshow(img, cmap="gray")
-    plt.imshow(gt, alpha=0.2)
-    plt.show()
+    # if params.predict:
+    #     patient = ds[random.randint(0, len(ds) - 1)]
+    #     instant = patient.views[View.A4C]
+    #     img = instant.img_proc
+    #     gt = instant.gt_proc
+    #     print("Image shape: {}".format(img.shape))
+    #     print("GT shape: {}".format(gt.shape))
+    #     print("ID: {}".format(patient.id))
+    #
+    #     slice = random.randint(0, len(img) - 1)
+    #     img = img[slice].squeeze()
+    #     gt = gt[slice]
+    # else:
+    #     sample = ds[random.randint(0, len(ds) - 1)]
+    #     img = sample[CamusTags.img].squeeze()
+    #     gt = sample[CamusTags.gt]
+    #     print("Image shape: {}".format(img.shape))
+    #     print("GT shape: {}".format(gt.shape))
+    #
+    # print(img.min())
+    # print(img.max())
+    # print(img.mean())
+    # print(img.std())
+    #
+    # f, (ax1, ax2) = plt.subplots(1, 2)
+    # ax1.imshow(img)
+    # ax2.imshow(gt)
+    # plt.show(block=False)
+    #
+    # plt.figure(2)
+    # plt.imshow(img, cmap="gray")
+    # plt.imshow(gt, alpha=0.2)
+    # plt.show()
